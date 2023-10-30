72a2df73df8acbf4b0db5891419ba7290adb7a11
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2023-10-30 20:29:55 +1100

    wip checkin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArraysComponent } from './arrays/arrays.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: ArraysComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
