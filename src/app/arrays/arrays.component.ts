df2cd992ce1dd6f5b30d4d4bb8d08ad57584a7af
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2023-10-31 19:52:27 +1100

    wip checkin

--- src/app/arrays/arrays.component.ts
+++ src/app/arrays/arrays.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-arrays',
   templateUrl: './arrays.component.html',
-  styleUrls: ['./arrays.component.css']
+  styleUrls: ['./arrays.component.css'],
 })
 export class ArraysComponent implements OnInit {
+  example = [1, 2, 3, 4, 5];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
