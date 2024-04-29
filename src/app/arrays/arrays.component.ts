93c5ed9ea19a8596cda3bfc35a95a4cd1a197fa7
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2024-04-29 20:26:58 +1000

    wip checkin

--- src/app/arrays/arrays.component.ts
+++ src/app/arrays/arrays.component.ts
@@ -8,12 +8,65 @@ import { Component, OnInit } from '@angular/core';
 export class ArraysComponent implements OnInit {
   example = [1, 2, 3, 4, 5];
   product?: number[];
+
+  prefix?: number[];
+  suffix?: number[];
   constructor() {}
 
   ngOnInit() {
     setTimeout(() => {
-      this.product = this.getProductUsingDivision(this.example);
+      this.product = this.getProductNotUsingDivision(this.example);
     }, 1000);
+  }
+
+  private getProductNotUsingDivision(arr: number[]): number[] {
+    var product: number[] = [];
+    console.log({ arr });
+    console.group('sumForward');
+    this.prefix = [];
+    for (let i = 0; i < arr.length; i++) {
+      let num = arr[i];
+      if (i === 0) {
+        this.prefix.push(num);
+      } else {
+        this.prefix.push(num * arr[i - 1]);
+      }
+    }
+    console.groupEnd();
+    console.log({ sumForward: this.prefix });
+
+    console.group('sumReversed');
+    this.suffix = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+      let num = arr[i];
+      if (i === arr.length - 1) {
+        this.suffix.push(num);
+      } else {
+        this.suffix.push(num * arr[i + 1]);
+      }
+      console.log({
+        i,
+        num,
+        sumReversed: JSON.parse(JSON.stringify(this.suffix)),
+      });
+    }
+    console.groupEnd();
+    console.log({ sumReversed: this.suffix });
+    this.suffix = this.suffix.reverse();
+    console.log({ sumReversed: this.suffix });
+
+    for (let i = 0; i < arr.length; i++) {
+      let num = arr[i];
+      if (i === 0) {
+        product.push(this.suffix[i + 1]);
+      } else if (i === arr.length - 1) {
+        product.push(this.prefix[i - 1]);
+      } else {
+        product.push(this.prefix[i - 1] * this.suffix[i + 1]);
+      }
+    }
+
+    return product;
   }
 
   private getProductUsingDivision(arr: number[]): number[] {
@@ -23,8 +76,40 @@ export class ArraysComponent implements OnInit {
       finalResult *= arr[i];
     }
 
+    console.log({ arr });
+    console.group('sumForward');
+    this.prefix = [];
     for (let i = 0; i < arr.length; i++) {
-      product.push(finalResult/arr[i]);
+      let num = arr[i];
+      if (i === 0) {
+        this.prefix.push(num);
+      } else {
+        this.prefix.push(num * arr[i - 1]);
+      }
+    }
+    console.groupEnd();
+    console.log({ sumForward: this.prefix });
+
+    console.group('sumReversed');
+    this.suffix = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+      let num = arr[i];
+      if (i === arr.length - 1) {
+        this.suffix.push(num);
+      } else {
+        this.suffix.push(num * arr[i + 1]);
+      }
+      console.log({
+        i,
+        num,
+        sumReversed: JSON.parse(JSON.stringify(this.suffix)),
+      });
+    }
+    console.groupEnd();
+    console.log({ sumReversed: this.suffix });
+
+    for (let i = 0; i < arr.length; i++) {
+      product.push(finalResult / arr[i]);
     }
     return product;
   }
@@ -33,7 +118,7 @@ export class ArraysComponent implements OnInit {
     var product = [];
     var value = arr[0];
     for (let i = 1; i < arr.length; i++) {
-      value =  value * arr[i];
+      value = value * arr[i];
       product.push(value);
       value = arr[i];
     }
@@ -48,7 +133,7 @@ export class ArraysComponent implements OnInit {
       console.group(`i=${i},result=${result}`);
       for (let j = 0; j < arr.length; j++) {
         if (i === j) {
-          console.log('i === j', {i, j});
+          console.log('i === j', { i, j });
           continue;
         }
         let res = arr[i] * arr[j];
